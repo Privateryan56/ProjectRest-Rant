e59d1c9684bc4f940697ea0306a974571487c19a
Austin bambino <link9215@gmail.com>
2022-03-26 10:36:46 -0400

    ok now /places is fixed

--- index.js
+++ index.js
@@ -5,27 +5,15 @@ const app = express()
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
-
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
-    res.render('places/index', {places})
+  res.render('home')
 })
 
-app.get('*', (req,res) =>{
+
+
+app.get('*', (req,res) => {
     res.status(404).render('error404')
 })
 
