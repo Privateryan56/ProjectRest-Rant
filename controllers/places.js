ff6da35a54bcd8b88532a35dbc332c26928a9842
Austin bambino <link9215@gmail.com>
2022-03-28 16:20:55 -0400

    can now add pictures of attractive men

--- controllers/places.js
+++ controllers/places.js
@@ -1,40 +1,35 @@
 const router = require('express').Router()
+const places = require('../models/places')
 
 
 // GET /new
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
+  //POST
   router.post('/new', (req, res) => {
       res.send('POST /new')
     })
 
   //GET INDEX
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/thaifood.jpg',
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/ricardo cat.jpg'
-    }, {
-        name: 'Placeholder',
-        city: 'Placeholder',
-        state: 'Placeholder',
-        cuisines: 'Placeholder',
-        pic: 'https://placekitten.com/250/250'
-    }]
     res.render('places/index', {places})
 })
+//POST 
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
 
 
---
+++ models/places.js
@@ -0,0 +1,19 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/thaifood.jpg',
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/ricardo cat.jpg'
+}, {
+    name: 'Placeholder',
+    city: 'Placeholder',
+    state: 'Placeholder',
+    cuisines: 'Placeholder',
+    pic: 'https://placekitten.com/250/250'
+}]
