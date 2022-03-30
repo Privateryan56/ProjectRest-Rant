0253ee9db179f5269629d7bf3b4bf519f42c4ae6
Austin bambino <link9215@gmail.com>
2022-03-30 19:29:49 -0400

    show Route Connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,36 +1,72 @@
 const router = require('express').Router()
-const places = require('../models/places')
+const places = require('../models/places.js')
 
-
-// GET /new
-router.get('/new', (req, res) => {
-    res.render('places/new')
-  })
-  //POST
-  router.post('/new', (req, res) => {
-      res.send('POST /new')
-    })
-
-  //GET INDEX
+//INDEX route
 router.get('/', (req, res) => {
-    res.render('places/index', {places})
+  res.render('places/index', {places})
 })
-//POST 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    if (!req.body.pic) {
-      // Default image if one is not provided
-      req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-      req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-      req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.redirect('/places')
-  })
 
+//NEW Route
+router.get('/new', (req,res)=>{
+  res.render('places/new')
+})
+
+//DELETE Route
+router.delete('/places/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(req.params.id, 1)
+    res.status(303).redirect('/places')
+  }
+})
+
+//POST Route
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+
+//SHOW route
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id], id })
+  }
+})
+
+//EDIT Route
+router.get('/:id/edit', (req,res)=>{
+  let id = Number(req.params.id)
+  if (isNaN(id)){
+    res.render('error404')
+  } else if (!places[id]) {
+    res.render('error404')
+  } else {
+    res.render('places/edit', {place: places[id]})
+  }
+})
 
 module.exports = router
