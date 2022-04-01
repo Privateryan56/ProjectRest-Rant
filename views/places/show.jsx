6a66f9478e654431a14efe1c39883f9f46ab5e92
Austin bambino <link9215@gmail.com>
2022-04-01 11:57:02 -0400

    starting to do edit route

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -19,7 +19,7 @@ function show(data) {
         <hr />
         <h2>Comments</h2>
         <p> No comments yet!</p>
-        <a href={`/places/${data.id}/error404`} className="btn btn-warning">
+        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
           Edit{" "}
         </a>
         <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
