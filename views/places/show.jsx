6c309e49f70ccf5fbf16521ef9cb848ff789ce16
Austin bambino <link9215@gmail.com>
2022-04-05 20:20:42 -0400

    added sum decorum

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,28 +5,37 @@ function show(data) {
   return (
     <Def>
       <main>
-        <h1>{data.place.name}</h1>
-        <img src={data.place.pic} alt={data.place.name} />
-        <h2>Rating</h2>
-        <p>Currently unrated</p>
-        <h2>Description</h2>
-        <p>
-          {" "}
-          Located in {data.place.city}, {data.place.state} and serving{" "}
-          {data.place.cuisines}
-        </p>
-        <br />
-        <hr />
-        <h2>Comments</h2>
-        <p> No comments yet!</p>
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit{" "}
-        </a>
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete{" "}
-          </button>
-        </form>
+        <div className="row">
+          <div className="col-sm-6">
+            <h1>{data.place.name}</h1>
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <h2>Rating</h2>
+          <p>Currently unrated</p>
+          <h2>Description</h2>
+          <h3>{data.place.showEstablished()}</h3>
+          <h4>Serving {data.place.cuisines}</h4>
+          <p>
+            {" "}
+            Located in {data.place.city}, {data.place.state} and serving{" "}
+            {data.place.cuisines}
+          </p>
+          <br />
+          <hr />
+          <h2>Comments</h2>
+          <p> No comments yet!</p>
+          <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+            Edit{" "}
+          </a>
+          <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+            <button type="submit" className="btn btn-danger">
+              Delete{" "}
+            </button>
+          </form>
+        </div>
       </main>
     </Def>
   );
