3369344f9983212fc8f2a23e65d9372ba9197aba
Austin bambino <link9215@gmail.com>
2022-04-05 20:25:34 -0400

    cleaned up some code bc mongoose doin da hgeavy liftin

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -5,8 +5,7 @@ function new_form() {
   return (
     <Def>
       <main>
-        <h1>Add a new place</h1>
-        <h2> place</h2>
+        <h1>Add a New Place</h1>
         <form method="POST" action="/places">
           <div className="form-group">
             <label htmlFor="name">Place Name</label>
@@ -32,16 +31,16 @@ function new_form() {
               name="cuisines"
               required
             />
-          </div>
-          <div className="form-group">
-            <label htmlFor="founded">Founded Year</label>
-            <input
-              type="number"
-              className="form-control"
-              id="founded"
-              name="founded"
-              value={new Date().getFullYear}
-            />
+            <div className="form-group col-sm-4">
+              <label htmlFor="founded">Founded Year</label>
+              <input
+                type="number"
+                className="form-control"
+                id="founded"
+                name="founded"
+                value={new Date().getFullYear()}
+              />
+            </div>
           </div>
           <input className="btn btn-primary" type="submit" value="Add Place" />
         </form>
